62a34c038711c5d1d61e1daa909c1a50b05358cc
Benedek Krisztin <b0kriben@vasvari.org>
2024-11-16 13:20:27 +0100

    MainWindow.xaml.cs ver1.0

--- ClearDive.desktop/ClearDive.desktop/MainWindow.xaml.cs
+++ ClearDive.desktop/ClearDive.desktop/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+//using ClearDive.viewmodel;
 
 namespace ClearDive.desktop
 {
@@ -19,6 +20,7 @@ namespace ClearDive.desktop
         public MainWindow()
         {
             InitializeComponent();
+            //this.DataContext = new MainViewModel();
         }
     }
 }
